df8e4bfd0d52ec40719c57b81ded56cde4cacc65
adnantariq123 <ahmedadnan123@hotmail.com>
2023-01-30 16:09:37 -0500

    edits

--- design-challenge/src/App.js
+++ design-challenge/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,26 +6,53 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function App() {
   return (
    <>
-    <Container fluid>
-      <Row className='bkGrey'>
+    <Container>
+      <Row >
       
-        <Col sm={12}><h2 className='isOrange'>Orange People</h2></Col>
+        
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Orange People</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+
+            <NavDropdown title="About" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Blog</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Community</NavDropdown.Item>
+
+            </NavDropdown>
+            <Nav.Link href="#home">Service</Nav.Link>
+            <Nav.Link href="#link">Solutions</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      <Col sm={2} className="CenterFlex"><Button variant="info">FIND YOUR JOB</Button></Col>
+    </Navbar>
+
+        
         
       </Row>
       <Row className="justify-content-md-center Splash">
-        <Col><h1>We strengthen our clients' capacity to solve complex business problems. </h1></Col>
+        <Col><span className='Bared'><h1>We strengthen our clients' capacity to solve complex business problems. </h1></span></Col>
       </Row>
 
       <Row className='bkGrey'>
-      <Col sm={6}></Col>
+      <Col sm={6} className="CenterFlex"><h3 className='WhitePLan'>Our technology advisors and consultants are problem definers, solvers, simplifiers, impartial sounding boards, and sparring partners.</h3></Col>
       <Col sm={6}>
         <div className='whiteBg'>
 
-        <Accordion defaultActiveKey="0">
+        <Accordion flush> 
       <Accordion.Item eventKey="0">
         <Accordion.Header>Who we are</Accordion.Header>
         <Accordion.Body>
@@ -41,31 +68,48 @@ function App() {
       <Accordion.Item eventKey="1">
         <Accordion.Header>How we can help</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. 
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="2">
         <Accordion.Header>Join us!</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          pariatur. 
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>Micropersonalization</Accordion.Header>
+        <Accordion.Body>
+        Sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. 
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
         </div>
       </Col>
       
+    </Row>
+
+    <Row>
+    <Col sm={6}></Col>
+    
+
     </Row>
     </Container>
    
